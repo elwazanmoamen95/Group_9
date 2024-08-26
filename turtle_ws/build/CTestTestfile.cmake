b3028f52a4ffebacf0fbac5692a0c298acbf29a7
elwazanmoamen95 <elwazanmoamen@gmail.com>
2024-08-26 05:03:57 +0300

    create pkg turtle_battle and make dir scripts for 2 Nodes game_engine and turtle_controller

---
+++ turtle_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/moa/Group_9/turtle_ws/src
+# Build directory: /home/moa/Group_9/turtle_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
