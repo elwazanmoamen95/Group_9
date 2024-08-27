d2be3d086fcd0a20f6bfbf5cd8da5a06092016c6
seifmaazouz <154283002+seifmaazouz@users.noreply.github.com>
2024-08-27 10:59:24 +0300

    Update game_engine.py
    cleaned up code, removed unnecessary subscription to /game_status, changed rate of updating turtle subscriptions to be more efficient, integrated updating of player status in publish_game_status and fixed some bugs

--- turtle_ws/src/turtle_battle/scripts/game_engine.py
+++ turtle_ws/src/turtle_battle/scripts/game_engine.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import rospy
+import time
 from turtlesim.msg import Pose
 from std_msgs.msg import String
 from std_msgs.msg import Int32MultiArray
@@ -10,20 +11,24 @@ class GameEngine:
     def __init__(self):
         rospy.init_node('game_engine')
         
-        self.player_statuses = {} # [turtleID]: health: x, attacks_left x
+        self.player_statuses = {} # [turtleID]: {'health': x, 'attacks_left': x}
         # Int32MultiArray: (turtleID1, health1, attacks_left1, turtleID2, health2, ...)
         self.status_pub = rospy.Publisher('/game_status', Int32MultiArray, queue_size=10) 
-        self.status_sub = rospy.Subscriber('/game_status', Int32MultiArray, self.status_callback)
         
         self.turtle_subs = {}
         self.attack_sub = rospy.Subscriber('/turtle_attack', String, self.attack_callback)
         
-        rate = rospy.Rate(10)
+
+        self.update_interval = 5  # Updates turtle subscriptions every 5 seconds
+        self.last_update_time = time.time()
+
+        self.rate = rospy.Rate(10)
+        rospy.loginfo("Game Engine has been created")
+        self.publish_game_status()
+        self.update_turtle_subscriptions()
+        self.run()
         
-        while not rospy.is_shutdown():
-            self.update_turtle_subscriptions() # constantly updates the number list of turtles
-            self.publish_game_status() # constantly updates the status of all turtles (health, attacks left..)
-            rate.sleep()
+
 
     def get_active_turtles(self):
         # Retrieve all topics
@@ -46,22 +51,37 @@ class GameEngine:
     def initialize_player_status(self, player_id):
         # Initialize status for a new player (turtle)
         if player_id not in self.player_statuses:
-            self.player_statuses[int(player_id)] = {
+            self.player_statuses[player_id] = {
                 'health': 100,  # Default initial health
                 'attacks_left': 10  # Default initial attacks left
             }
             rospy.loginfo(f"Initialized status for turtle {player_id}")
             self.publish_game_status()
 
-    def publish_game_status(self):
+    def publish_game_status(self, update_id = None, update_health = None, update_attacks = None):
         status_msg = Int32MultiArray()
         data = []
-
+        
+        
+             
         for player_id, status in self.player_statuses.items():
+
             # Check and cast types
             player_id = int(player_id)
             health = int(status['health'])
             attacks_left = int(status['attacks_left'])
+            
+            #check if update needed
+            if update_id == player_id:
+                if update_health != None:
+                    health = update_health
+                if update_attacks != None:
+                    attacks_left = update_attacks
+
+            self.player_statuses[player_id] = {
+                'health': health,
+                'attacks_left': attacks_left
+            }
 
             # Append data
             data.extend([player_id, health, attacks_left])
@@ -71,56 +91,45 @@ class GameEngine:
         
         rospy.loginfo(f"Publishing game status: {status_msg.data}")
         self.status_pub.publish(status_msg)
-    
 
 
     # <-- CALLBACK FUNCTIONS -->
     def pose_callback(self, msg: Pose):
         # mechanism to detect if a new turtle has joined.
-        turtle_name = str(msg._connection_header['topic'].split('/')[1])
+        turtle_name = str(msg._connection_header['topic']).split('/')[1]
         _, player_id = turtle_name.split("turtle")
+        player_id = int(player_id)
 
         if player_id not in self.player_statuses:
-            rospy.loginfo(f"Initializing status for new turtle: {player_id}")
+            # rospy.loginfo(f"Initializing status for new turtle: {player_id}")
             self.initialize_player_status(player_id)
 
     def attack_callback(self, msg: String):
         turtle_name = msg.data
-        rospy.loginfo(f"Turtle {turtle_name} performed an attack!") 
+        rospy.loginfo(f"{turtle_name} performed an attack!") 
         # still need to implement attack logic
         # --> to do <--
-
-    def status_callback(self, msg: Int32MultiArray):
-        # Get the number of active turtles (players)
-        num_players = len(self.get_active_turtles())
-
-        data = msg.data
-        expected_length = num_players * 3
         
-        # for debugging
-        rospy.loginfo(f"Received data: {data}")
-        rospy.loginfo(f"Expected length: {expected_length}, Actual length: {len(data)}")
-
-
-        # Check if the data length matches the expected length
-        if len(data) != expected_length:
-            rospy.logwarn("Received status message with unexpected length.")
-            return
+        # for testing purpose
+        _, player_id = turtle_name.split("turtle")
+        player_id = int(player_id)
+        curr_attacks = int(self.player_statuses[player_id]['attacks_left'])
+        self.publish_game_status(update_id=player_id, update_attacks=curr_attacks-1) 
         
-        # Process the data in chunks of 3 (id, health, attacks left)
-        for i in range(0, expected_length, 3):
-            player_id = int(data[i])
-            health = int(data[i + 1])
-            attacks_left = int(data[i + 2])
 
-            # Update player status
-            self.player_statuses[player_id] = {'health': health, 'attacks_left': attacks_left}
-            rospy.loginfo(f"Updated Status - Turtle {player_id}: Health={health}, Attacks Left={attacks_left}")
+    def run(self):
+        while not rospy.is_shutdown():
+            current_time = time.time()
+            if current_time - self.last_update_time > self.update_interval:
+                self.update_turtle_subscriptions() # updates the number list of turtles every {self.update_interval} seconds
+                self.last_update_time = current_time
+            
+            rospy.loginfo(f"Status: {self.player_statuses}") # for debug
+            self.rate.sleep()
 
 
 if __name__ == '__main__':
     try:
         GameEngine()
-        rospy.spin()
     except rospy.ROSInterruptException:
         pass
