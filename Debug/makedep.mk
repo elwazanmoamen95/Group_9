cbfa1805a873ea3d65bcbaf82fbfa7fc2e035d11
marwanviper <125487941+marwanviper@users.noreply.github.com>
2024-09-02 23:29:37 +0300

    adding debug file include the hex file

---
+++ Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+ADC.c
+
+ext_interrupt.c
+
+GPIO.c
+
+main.c
+
+spi.c
+
+timer.c
+
